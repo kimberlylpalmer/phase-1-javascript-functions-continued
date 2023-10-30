514bb25a92f8307a11c3a106d1d652ce5f8b3903
Kimberly Palmer <kimberlylpalmer@gmail.com>
2023-10-30 04:43:38 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork = function(activity2 = 'go to the office') {
+    return `This Monday, I will ${activity2}.`
+}
+
+
+
+
+function wrapAdjective(symbol = "*") {
+    return function(adjective = "special") {
+        return `You are ${symbol}${adjective}${symbol}!`
+    }
+}
+ 
